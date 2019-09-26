3c460ab0c269ba505b27b30d4c70eeaa49d24d9d
adriacabeza <adriacabeza@hotmail.com>
2019-09-26 11:12:40 +0200

    :card_file_box: Dataset finished
    gonna try everything in google colab

--- google_images.py
+++ google_images.py
@@ -1,20 +1,20 @@
 import os
 import json
-#from google_images_download import google_images_download   #importing the library
+from google_images_download import google_images_download   #importing the library
 
-#response = google_images_download.googleimagesdownload()
+response = google_images_download.googleimagesdownload()
 names_list = os.listdir('scraper/mushroom_img')
 names_set = set()
 for x in names_list:
     names_set.add(x.split('.')[0][:-1])
 
 print('Number of classes inside our dataset: {}'.format(len(names_set)))
-"""
+
 arguments= {"keywords":"{}".format(','.join(names_set)), "limit":20,"print_urls":True}
 absolute_image_paths = response.download(arguments)
 print(absolute_image_paths)
-"""
 prefix = './downloads'
+
 if not os.path.exists("./dataset"):
     os.mkdir('./dataset')
 for x in names_set:
@@ -23,4 +23,6 @@ for x in names_set:
     images_list_scrapy = [os.path.join('scraper/mushroom_img/',j) for j in names_list if j.startswith(x)]
     images_list = images_list + images_list_scrapy
     for num,i in enumerate(images_list):
-        os.rename(i,os.path.join('./dataset',x,'_',+str(num)+'.jpg'))
+        if not os.path.exists(os.path.join('./dataset',x)):
+            os.mkdir(os.path.join('./dataset',x))
+        os.rename(i,os.path.join('./dataset',x,x+'_'+str(num)+'.jpg'))
