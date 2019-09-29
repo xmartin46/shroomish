53d590b88c7415e4800b85f64a59b89b894799c3
adriacabeza <adriacabeza@hotmail.com>
2019-09-29 16:44:19 +0200

    :iphone: New gallery of mushrooms being created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomepageImage from './components/HomepageImage'
-//import NavBar from './components/NavBar'
 //import MushroomList from './components/MushroomList'
 import PrimarySearchAppBar from './components/PrimarySearchAppBar'
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./components/photos";
+
+
+
 
 function App() {
+	 const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div className="App">
 	  <div>
 	  <PrimarySearchAppBar/>
 	  </div>
-      <header className="App-header">
-      <HomepageImage/>
-	<p>
-        Mushroom Classifier
-	</p>
+	<header className="App-header">
+	     <div>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
 	  </header>
     </div>
   );
